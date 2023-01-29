63b699bee45ec0362d21d9705f11506fedc1521e
unknown <miodragkruscic02@gmail.com>
2023-01-29 23:47:03 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let dugmad = document.querySelectorAll('button');
+
+
+
+console.log(dugmad);
+
+
+dugmad.forEach(dugme => {
+    dugme.addEventListener("click", function(){
+        console.log("wow")
+    })
+});
+
+
+
